9407a88bae57699f4bf9462ee35045771236b230
hp <hp@DESKTOP-4JSGQT6>
2024-06-05 11:36:21 +0530

    Access modifier with another pack

---
+++ firstmavenproject/src/main/java/accessmodifiersample/Example2_Modifier.java
@@ -0,0 +1,17 @@
+package accessmodifiersample;
+
+import acessmodifiers.Example_Modifier;
+
+public class Example2_Modifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example_Modifier obj2=new Example_Modifier();
+		obj2.display1();
+		//obj2.display3(); protected access method within another package
+		//obj2.display4(); default access method within another 
+		
+
+	}
+
+}
