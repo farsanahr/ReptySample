a271de60f43c6746e5d43bc52594d99e13df4564
hp <hp@DESKTOP-4JSGQT6>
2024-06-05 10:46:49 +0530

    modifierfirstexample

---
+++ firstmavenproject/src/main/java/acessmodifiers/Example_Modifier.java
@@ -0,0 +1,31 @@
+package acessmodifiers;
+
+public class Example_Modifier {
+
+	public static void main(String[] args) {
+		Example_Modifier obj=new Example_Modifier();
+		obj.display1();
+		obj.display2();
+		obj.display3();
+		obj.display4();
+	}
+		// TODO Auto-generated method stub
+     public void display1()
+     {
+    	 System.out.println("It is a public modifier");    
+    	 }
+     private void display2()
+     {
+    	 System.out.println("Its a private modifier");
+     }
+     protected void display3()
+     {
+    	 System.out.println("Its a protected modifier");
+     }
+     void display4()
+     {
+    	 System.out.println("Its a default modifier");
+     }
+	
+
+}
